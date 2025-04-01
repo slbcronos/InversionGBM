ce4af36abe565cd13baed40466f466c3cdddabc3
Salvador LB <slb_18@hotmail.com>
2025-04-01 01:33:31 -0600

    Class Conexion

---
+++ src/inversiongbm/Conexion.java
@@ -0,0 +1,14 @@
+/*
+ * INSERT INTO `interesGBM` (`id`, `interes`) VALUES (NULL, ''), (NULL, '6.50');
+ */
+package inversiongbm;
+
+/**
+ *
+ * @author salvadorlopezbaltazar
+ */
+public class Conexion {
+    //Conexion a Base de Datos con el interes Actual de GBM
+    
+    
+}
